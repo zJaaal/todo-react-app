c1e142e8b2adf75a0f457ce105442a4959f76d79
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-26 14:10:32 -0300

    Initial state of the project
    Added bootstrap
    Changed the template

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h3>Hello World!</h3>;
-}
-
-export default App;
---
+++ src/TodoApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoApp = () => {
+  return (
+    <div className="d-flex justify-content-center mt-2">
+      <h1>TodoApp</h1>
+    </div>
+  );
+};
+
+export default TodoApp;
